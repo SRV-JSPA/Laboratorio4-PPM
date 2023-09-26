f2e5448ffd6125dcd760f3b108251e4bbd0df979
JSPA <per22318@uvg.edu.gt>
2023-09-26 08:34:57 -0600

    bienvenida al app

--- app/src/main/java/com/example/laboratorio4/App.kt
+++ app/src/main/java/com/example/laboratorio4/App.kt
@@ -3,13 +3,18 @@ package com.example.laboratorio4
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.laboratorio4.ui.theme.Laboratorio4Theme
 
 class App : ComponentActivity() {
@@ -22,7 +27,7 @@ class App : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting2("Android")
+                    app()
                 }
             }
         }
@@ -30,17 +35,24 @@ class App : ComponentActivity() {
 }
 
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun app() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "¡Bienvenido al app!",
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview3() {
     Laboratorio4Theme {
-        Greeting2("Android")
+        app()
     }
 }
